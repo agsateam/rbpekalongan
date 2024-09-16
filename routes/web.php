1f3c525247a0cb0d0025b6a7e58240ef726362bf
vardrz <fatkhurrozakf@gmail.com>
2024-09-16 17:42:58 +0700

    feat: events page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BerandaController::class, 'index']);
+Route::get('/event', [EventController::class, 'front']);
