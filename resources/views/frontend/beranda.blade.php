63bf3e9037171618472bf6780eb3174fa6b331bf
ikkromm18 <muhammadikkrom749@gmail.com>
2024-09-16 15:58:28 +0700

    fix: memecah hero section menjadi komponen

--- resources/views/frontend/beranda.blade.php
+++ resources/views/frontend/beranda.blade.php
@@ -2,55 +2,9 @@
 @section('title', 'Beranda')
 @section('content')
 
-    <div class="w-full lg:max-w-screen-xl md:w-screen lg:mx-14 2xl:mx-auto flex justify-center">
-        <div class="flex flex-col xl:flex-row items-center justify-center mt-10">
-            <div class="flex flex-col w-1/2">
-                <div class="">
-                    <img src="./images/logorumahbumn.png" alt="Logo Rumah Bumn">
-                    <h3 class="font-manrope text-center text-2xl font-bold md:text-5xl md:text-left">Kota Pekalongan</h3>
-                    <p class="text-center mt-2 md:mr-5 md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Voluptatem, nostrum
-                        dolorem
-                        dicta amet vitae
-                        asperiores maxime nesciunt, consectetur quae, omnis neque a id architecto veniam. Reiciendis
-                        sapiente placeat aliquid incidunt minima qui amet at illo porro. Molestias consequatur sed
-                        blanditiis recusandae voluptatem, porro assumenda velit reprehenderit laborum tenetur non pariatur
-                        unde quo nam quia hic, placeat quasi, illo perspiciatis a! Officiis quos dolorum vel, perferendis
-                        necessitatibus voluptatibus voluptatem odio rem.</p>
-                </div>
-            </div>
-            <div class="flex justify-center items-center w-1/2">
-                <!-- Slider main container -->
-                <div class="swiper w-96 md:w-[600px] xl:w-[600px] h-full">
-                    <!-- Additional required wrapper -->
-                    <div class="swiper-wrapper mt-8">
-                        <!-- Slides -->
-                        <div class="swiper-slide"><img src="https://random.imagecdn.app/v1/image?" alt="fofo 1">
-                        </div>
-                        <div class="swiper-slide"><img src="https://random.imagecdn.app/v1/image?" alt="fofo 1">
-                        </div>
-                        <div class="swiper-slide"><img src="https://random.imagecdn.app/v1/image?" alt="fofo 1">
-                        </div>
-                        <div class="swiper-slide"><img src="https://random.imagecdn.app/v1/image?" alt="fofo 1">
-                        </div>
-                        <div class="swiper-slide"><img src="https://random.imagecdn.app/v1/image?" alt="fofo 1">
-                        </div>
 
-                    </div>
-                    <!-- If we need pagination -->
-                    <div class="swiper-pagination"></div>
-
-                    <!-- If we need navigation buttons -->
-                    <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div>
-
-                    <!-- If we need scrollbar -->
-                    {{-- <div class="swiper-scrollbar"></div> --}}
-                </div>
-            </div>
-
-        </div>
-    </div>
+    <x-beranda.hero />
+    <x-beranda.services />
 
 
     <x-beranda.testimonies />
