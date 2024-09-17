7c41d412d148c238b59a8beadcd42a55de0aa27a
ikkromm18 <muhammadikkrom749@gmail.com>
2024-09-17 20:22:34 +0700

    feat: section statistik

--- resources/views/frontend/beranda.blade.php
+++ resources/views/frontend/beranda.blade.php
@@ -5,8 +5,7 @@
 
     <x-beranda.hero />
     <x-beranda.services />
-
-
+    <x-beranda.statistik />
     <x-beranda.upcomingEvent />
     <x-beranda.testimonies />
     <x-beranda.activities :igposts="$igPosts" />
