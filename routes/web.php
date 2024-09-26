fd43c24965c93a32c25f450112d70056a6d512a1
ikkromm18 <muhammadikkrom749@gmail.com>
2024-09-26 18:39:02 +0700

    commit untuk melakukan pull request

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,8 @@ Route::middleware('auth')->group(function () {
     // Web Content
 
     // Fungsi
-    Route::get('/webcontent/fungsi', [FungsiController::class, 'index'])->name('webcontent.fungsi');
+
+    Route::prefix('webcontent')->group(function () {
+        Route::get('fungsi', [FungsiController::class, 'index'])->name('webcontent.fungsi');
+    });
 });
