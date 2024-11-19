b6c18f1d93dd018e7a2ae124623985d1e74265fb
ikkromm18 <muhammadikkrom749@gmail.com>
2024-11-19 17:08:55 +0700

    fix: disabled jumlah event in statistik BE

--- resources/views/backend/webcontent/statistik/viewstatistik.blade.php
+++ resources/views/backend/webcontent/statistik/viewstatistik.blade.php
@@ -95,9 +95,9 @@
 
                     <div class="flex mt-4 md:mt-6">
 
-                        <a href="{{ route('webcontent.statistik.getdata', 4) }}"
+                        {{-- <a href="{{ route('webcontent.statistik.getdata', 4) }}"
                             class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Lihat
-                            Detail</a>
+                            Detail</a> --}}
 
                     </div>
                 </div>
